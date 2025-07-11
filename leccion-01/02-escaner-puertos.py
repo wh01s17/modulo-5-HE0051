db16e4e1ff79fb8444eddbe516d0249bb362378c
wh01s17 <jor.perezo@hotmail.com>
2025-07-10 20:37:02 -0400

    added comments

--- leccion-01/02-escaner-puertos.py
+++ leccion-01/02-escaner-puertos.py
@@ -1,11 +1,16 @@
 import socket
 
 host = "127.0.0.1"
-puertos = [21, 22, 80, 443, 3306]
+#             ftp, ssh, http, https, mysql/mariadb
+puertos_lista = [21, 22, 80, 443, 3306]
 
 for puerto in puertos:
     s = socket.socket()
-    s.settimeout(1)
+    s.settimeout(1)     # 1 segundo
+    # s.settimeout(0.5)   # medio segundo (500 milisegundos)
+    # s.settimeout(0.1)   # 100 milisegundos
+
     if s.connect_ex((host, puerto)) == 0:
         print(f"[+] Puerto {puerto} abierto")
+
     s.close()
---
+++ leccion-01/repaso.py
@@ -0,0 +1,65 @@
+# Variables
+nombre = 'Sustantiva'
+
+edad = 20
+
+activo = True
+
+decimal = 0.2
+
+lista = [1,2,3]
+
+diccionario = {"a" : 1}
+
+# Bucles
+# for i in range(5):
+#     print(i) # muestra numeros del 0 al 4
+
+# print('Hola mundo')
+
+# n = 0
+# while n < 5:
+#     print(n)
+#     n += 1
+
+# edad = 10
+# if edad >= 18:
+#     print("Eres mayor de edad")
+# elif edad >= 13:
+#     print("Eres adolescente")
+# else:
+#     print("Eres niño")
+
+# Operadores
+
+# comparacion
+# ==, !=, >, <, >=, <=
+
+# aritmeticos
+# +, -, *, /, %, **
+
+# logicos
+# and, or, not
+
+# edad = 18
+# so = 'Windows'
+# activo = True
+
+# if not activo:
+#     print('Activo')
+# else:
+#     print('No activo')
+
+# def saludar(nombre):
+#     print(f"Hola, {nombre}")
+
+# saludar('Jordy')
+
+# Operador ternario
+# edad = 20
+# estado = "Mayor" if edad >= 18 else "Menor"
+
+# print(estado)
+
+# import os
+# import requests
